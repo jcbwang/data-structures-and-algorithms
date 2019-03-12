cd79b5c75467caca0b48df6a02f3dee1c56c5b29
Bonnie Wang <jcwang118@hotmail.com>
2019-03-12 13:26:40 -0700

    finishing number 5

--- code-challenges/301/for-each/challenges-01.test.js
+++ code-challenges/301/for-each/challenges-01.test.js
@@ -93,8 +93,14 @@ This anonymous function should accept up to three arguments: the element, the in
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithAnon = (arr) => {
-  // Solution code here...
-}
+  arr.forEach((num)=>{
+    if(num % 3 === 2){
+      arr.pop();
+    }
+  });
+  return arr;
+};
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
